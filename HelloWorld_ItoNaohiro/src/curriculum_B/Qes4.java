f1ac15b751eeb1c54b1da4f9d0f29486db21f373
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2025-06-10 14:52:11 +0900

    1.10 for型反復処理 (課題4)

--- HelloWorld_ItoNaohiro/src/curriculum_B/Qes4.java
+++ HelloWorld_ItoNaohiro/src/curriculum_B/Qes4.java
@@ -3,8 +3,25 @@ package curriculum_B;
 public class Qes4 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		/**********************************************************************************
+		* [概要] 九九の表の作成
+		* [詳細] for文を用いて九九の表を作成する
+		**********************************************************************************/
+		
+		// 先頭の段の数字を1~9をそれぞれ出力
+		for (int i = 1; i <= 9; i++) {
+			// 先頭の列の数字を1~9をそれぞれ出力
+            for (int j = 1; j <= 9; j++) {
+                // 文字列multiplicationに格納する
+                String multiplication = String.format("%02d * %02d = %02d", i, j, i * j); // 2桁ゼロ埋めになるように各項目を整形
+                // "||"をつけて整形
+                if (j < 9) {
+                    System.out.print(multiplication + " || "); // 式の後に || を追加
+                } else {
+                    System.out.print(multiplication); // 9の列で改行する
+                }
+            }
+            System.out.println(); // 9の段で改行する
+        }
 	}
-
 }

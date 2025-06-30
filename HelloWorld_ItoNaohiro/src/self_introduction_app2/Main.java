4ad3dc65a1eb010d78f547eea116e797c8304d95
chapy0706 <0xeca447abc7168eab67d279eafd33f39da9b54e40@ethermail.io>
2025-07-01 02:50:34 +0900

    1.31 自己紹介プログラムを作成(問題1~6)

--- HelloWorld_ItoNaohiro/src/self_introduction_app2/Main.java
+++ HelloWorld_ItoNaohiro/src/self_introduction_app2/Main.java
@@ -8,5 +8,10 @@ public class Main{
 		  person1.print();
 		  Person person2 = new Person("山田花子", 22, 1.5, 40);
 		  person2.print();
+		  // 問題3：合計人数を出力する
+		  System.out.println("合計" + Person.count + "人です");
+		  
+		  // 問題6：合計人数を出力する(課題1.30と同じ)
+		  Person.printCount(); // 合計人数を出力（staticメソッド呼び出し）
 		 }
 		}
